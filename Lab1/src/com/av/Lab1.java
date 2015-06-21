fda576cdd8975494b0ce06df370cee1684e62966
Avnish Kumar <Avnish Kumar@192.168.100.6>
2015-06-21 20:18:45 +0530

    Initial Checking

---
+++ Lab1/src/com/av/Lab1.java
@@ -0,0 +1,16 @@
+package com.av;
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+		m1();
+
+	}
+	public static void m1(){
+		System.out.println("m1--First");
+		System.out.println("m1--second");
+		
+		
+	}
+
+}
